5ffd025570ac73710bb56ffe4bfd91392d280173
Brenda Monis Moreno <91157764+BrendaMonis@users.noreply.github.com>
2024-03-18 17:08:00 -0300

    Update mod_MixedModel.R

--- R/mod_MixedModel.R
+++ R/mod_MixedModel.R
@@ -7,6 +7,7 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
+
 mod_MixedModel_ui <- function(id){
   ns <- NS(id)
   tagList(
@@ -14,11 +15,15 @@ mod_MixedModel_ui <- function(id){
              box(width = 12, 
                  p("Run analysis with mixed models.")
              ),
+             
+             # Choose the experiment design
              box(width = 12,
                  selectInput(ns("design"), label = h4("Experiment design"), 
                              choices = list("Randomized complete block" = "block", "Alpha lattice" = "lattice"), 
                              selected = "block")
              ),
+             
+             # Input the file
              box(width = 12, solidHeader = TRUE, collapsible = TRUE, status="primary", title = "Input file",
                  p("The input file is a tab delimited table with a column called 'local' defining the environment, 
                    other called 'gen' defining the genotypes and other called 'block' defining the block number. 
@@ -27,9 +32,27 @@ mod_MixedModel_ui <- function(id){
                  p("Upload here your file:"),
                  fileInput(ns("data_input"), label = h6("File: data.txt"), multiple = F),
                  p("If you do not have an file to be upload you can still check this app features with our example file. The example file is automatically upload, you just need to procedure to the other buttons."),
-                 br(),
-                 actionButton(ns("read_data"), "Read the file",icon("refresh")), hr()
+                 
+                 #Input Control
+                 hr(),
+                 p("Data View:"),
+                 box(width = 4,
+                     radioButtons(ns("read_data1"), label = p("Select o separator"),
+                                  choices = list("Comma" = ",", "Semicolon" = ";", "Tab" = "\t"),
+                                  selected = ";")
+                 ),
+                 box(width = 8,  
+                     #title = "Database",
+                     #data visualisation
+                     tableOutput(ns("dataview"))
+                 ),
+                 # Read the file
+                 hr(),
+                 actionButton(ns("read_data"), "Read the file",icon("refresh")), 
+                 hr(),
              ),
+             
+             #Select variables
              box(width = 12, solidHeader = TRUE, collapsible = TRUE, status="primary", title = "Select variables",
                  box(width = 6,
                      radioButtons(ns("trait"), label = p("Choose the traits to be evaluated:"),
@@ -42,6 +65,8 @@ mod_MixedModel_ui <- function(id){
                                         selected = "Press 'Read the file' button to update")
                  )
              ),
+             
+             #Define the model
              box(width = 12, solidHeader = TRUE, collapsible = TRUE, status="primary", title = "Define the model:",
                  p("If you want to consider a pedigree matrix, please submit an csv file as the example:"),
                  downloadButton(ns("pedigree_example")), hr(),
@@ -52,9 +77,14 @@ mod_MixedModel_ui <- function(id){
                  textInput(ns("fixed"), label = p("Fixed:"), value = "phen1 ~ local"),
                  textInput(ns("random"), label = p("Random:"), value = "~ gen + local:block + local:gen"),
                  textInput(ns("rcov"), label = p("rcov:"), value = "~ units"), hr(),
+                 # radioButtons(ns("rcov"), label = p("Choose the rcov to be evaluated:"), hr(),
+                 #              choices = list("units" = "units", "vsr" = "vsr", "vsc" = "vsc"), 
+                 #              selected = "units"),
                  actionButton(ns("run_analysis"), "Run analysis",icon("refresh")), br(),
                  p("Expand the windows above to access the results")
              ), hr(),
+             
+             # Results
              box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Results:",
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, title = "Variance components:",
                      DT::dataTableOutput(ns("varcomp_out"))
@@ -62,7 +92,7 @@ mod_MixedModel_ui <- function(id){
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, title = "AIC and BIC",
                      DT::dataTableOutput(ns("aic_bic_out"))
                  ),
-                 box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, title = "AIC and BIC",
+                 box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, title = "BLUPs",
                      DT::dataTableOutput(ns("blups_out"))
                  ),
              )
@@ -93,6 +123,23 @@ mod_MixedModel_server <- function(input, output, session){
     } 
   )
   
+  # Data processing using .txt and .csv files.
+  observeEvent(input$read_data1, {
+    observeEvent(input$data_input, {
+      if (is.null(input$data_input)) {
+        output$dataview <- renderTable({
+          return(p("Upload your data"))
+        })
+      } else {
+        df <- read.csv(input$data_input$datapath, sep = input$read_data1)
+        output$dataview <- renderTable({
+          return(head(df))
+        })
+      }
+    })
+  })  
+  
+  
   ## download pedigree
   output$pedigree_example <- downloadHandler(
     filename =  function() {
@@ -106,15 +153,16 @@ mod_MixedModel_server <- function(input, output, session){
   )
   
   button1 <- eventReactive(input$read_data, {
-    if(is.null(input$data_input)){
+    if (is.null(input$data_input$datapath)) {
       if(input$design == "block"){
         dat <- read.csv(system.file("ext","example_inputs/example_blocks.csv", package = "StatGenESALQ"))
       } else {
         dat <- read.csv(system.file("ext","example_inputs/example_lattice.csv", package = "StatGenESALQ"))
       }
     } else {
-      dat <- read.csv(input$data_input)
+      dat <- read.csv(input$data_input$datapath, sep = input$read_data1)
     }
+    cat(colnames(dat))
     dat
   })
   
@@ -124,6 +172,7 @@ mod_MixedModel_server <- function(input, output, session){
       choices_trait_temp <- colnames(button1())[-c(1:4)] else
         choices_trait_temp <- colnames(button1())[-c(1:3)]
       
+      #Choose the trait and the location   
       choices_trait <- choices_trait_temp
       names(choices_trait) <- choices_trait_temp
       
@@ -142,6 +191,7 @@ mod_MixedModel_server <- function(input, output, session){
                                selected = unlist(choices_locations)[1:2])
   })
   
+  # defining the model as a factor
   button2 <- eventReactive(input$run_analysis, {
     withProgress(message = 'Building graphic', value = 0, {
       incProgress(0, detail = paste("Doing part", 1))
@@ -170,11 +220,13 @@ mod_MixedModel_server <- function(input, output, session){
       if(!is.null(input$pedigree)) A <- read.csv(input$pedigree$datapath, row.names = 1, header = T)
       
       
+      # Input the model
       mod <- mmer(fixed = as.formula(input$fixed), 
                   random = as.formula(input$random), 
                   rcov = as.formula(input$rcov),
                   data = dat)
       
+      # Results
       summary_mod <- summary(mod)
       
       aic_bic <- data.frame(AIC = mod$AIC, BIC = mod$BIC)
@@ -187,15 +239,32 @@ mod_MixedModel_server <- function(input, output, session){
     })
   })
   
-  output$varcomp_out <- DT::renderDataTable(
-    DT::datatable(data.frame(button2()[[2]]$varcomp),  
+  output$varcomp_out <- DT::renderDataTable({
+    data <- data.frame(button2()[[2]]$varcomp)
+    
+    # Especifique as colunas que deseja arredondar e o número de casas decimais
+    # columns_to_round <- c("Sum.Sq", "Mean.Sq", "F.value", "Pr..F.", "outra_coluna1", "outra_coluna2")
+    decimal_places1 <- 4  # Especifique o número de casas decimais
+    
+    # Arredonde as colunas selecionadas
+    for (col in 1:3) {
+      data[[col]] <- round(as.numeric(data[[col]]), decimal_places1)
+    }
+    
+    # decimal_places1 <- 5
+    # for (col in 5) {
+    #   data[[col]] <- round(as.numeric(data[[col]]), decimal_places1)
+    # }
+    
+    # Outputs
+    DT::datatable(data,  
                   extensions = 'Buttons',
                   options = list(
                     dom = 'Bfrtlp',
                     buttons = c('copy', 'csv', 'excel', 'pdf')
                   ),
                   class = "display")
-  )
+  })
   
   output$aic_bic_out <- DT::renderDataTable(
     DT::datatable(data.frame(button2()[[3]]),  
